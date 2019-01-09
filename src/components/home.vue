d25374e6eb7975676a26e0cd398ae29fd7e1f59a
zhu <983132370@qq.com>
2019-01-09 17:32:15 +0800

    页面修改

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,122 +1,64 @@
 <template>
   <div id="home">
     <navheader :activeIndex="activeIndex"></navheader>
-    <el-row type="flex" justify="center" style="margin-top:80px">
-      <el-col :span="7">
-        <el-input placeholder="请输入内容"></el-input>
-      </el-col>
-      <el-col :span="1">
-        <el-button icon="el-icon-search" type="primary">查询</el-button>
+    <el-row>
+      <el-col>
+        <el-row>
+          <el-col>
+            <el-carousel :interval="4000" type="card" height="280px" arrow="never">
+              <el-carousel-item v-for="item in rec_banners" :key="item">
+                <img width="100%" height="100%" :src="item.pic">
+              </el-carousel-item>
+            </el-carousel>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20"  >
+          <el-col :span="20" >
+            <div style="border: 1px rgba(128,128,128,0.14) solid"></div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20">
+          <el-col :span="20" >
+            <div class="h1 title" >电视剧</div>
+          </el-col>
+        </el-row>
+        <el-row :gutter="20" class="list">
+          <el-col :span="4" v-for="item in 12" :key="item">
+            <div class="grid-content bg-purple" style="margin-top: 10px">
+              <img src="http://puui.qpic.cn/vcover_vt_pic/0/924ic63thtubo491539913042/220" class="image">
+            </div>
+          </el-col>
+        </el-row>
       </el-col>
     </el-row>
-    <div>
-      <div class="content-pannel">
-        <el-card class="box-card">
-          <div slot="header" class="clearfix">
-            <span class="content-pannel-title">CAS - 企业级开源单点登录解决方案 </span>
-            <el-button type="primary" class="el-button--small " style="margin-left: 10px">emm</el-button>
-            <el-icon style="float: right; padding: 3px 8px" class="el-icon-star-off"></el-icon>
-            <el-icon style="float: right; padding: 3px 8px" class="el-icon-share"></el-icon>
-          </div>
-          <div slot="header" class="clearfix">
-            <span class="content-pannel-time">
-              <el-icon class="el-icon-time"></el-icon> 2018-01-01 发布
-            </span>
-          </div>
-          <div class="text item">
-            CAS - 企业级开源单点登录解决方案 荐CAS - 企业级开源单点登录解决方案 荐CAS - 企业级开源单点登录解决方案 荐CAS -
-            企业级开源单点登录解决方案 荐CAS -
-          </div>
-          <div class="el-table--border content-pannel-bottom">
-            <span>收藏 0 查看 0</span>
-          </div>
-        </el-card>
-      </div>
-
-      <div class="rank-pannel">
-        <el-card class="box-card" style="width: 300px;">
-          <div slot="header" class="clearfix">
-            <span>排行榜</span>
-            <!--<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>-->
-          </div>
-          <div class="text item">
-            <div>
-              <el-row type="flex" style="margin-bottom: 10px">
-                <el-col :span="5">
-                  <img src="http://pic.58pic.com/58pic/13/89/87/258PICU58PIC9gJ_1024.jpg"
-                       style="width: 40px;border-radius: 10px">
-                </el-col>
-                <el-col :span="7" style="padding-top: 18px">
-                  德玛西亚
-                </el-col>
-                <el-col :span="7" style="padding-top: 18px">
-                  <el-tag class="level-label" >布衣达人</el-tag>
-                </el-col>
-              </el-row>
-              <el-row type="flex" style="margin-bottom: 10px">
-                <el-col :span="5">
-                  <img src="http://pic.58pic.com/58pic/13/89/87/258PICU58PIC9gJ_1024.jpg"
-                       style="width: 40px;border-radius: 10px">
-                </el-col>
-                <el-col :span="7" style="padding-top: 18px">
-                  德玛西亚
-                </el-col>
-              </el-row>
-            </div>
-          </div>
-        </el-card>
-      </div>
-
-    </div>
 
   </div>
 </template>
 
 <style>
-  .level-label {
-    font-size: 12px;
-    position: relative;
-    float: right;
+  #home{
+    width:1400px;
+    margin:0 auto;
   }
-  .content-pannel {
-    margin: 50px 50px 0px 450px;
-    position: relative;
-    float: left;
+  .list img{
+    width:100%;
   }
-  .content-pannel-title {
-    font-size: 22px;
+  .title{font-size: 25px;padding-top: 5px}
+  .time {
+    font-size: 13px;
+    color: #999;
   }
-  .content-pannel-time {
-    color: rgba(0, 0, 0, 0.29);
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
   }
-  .content-pannel-bottom {
-    color: rgba(0, 0, 0, 0.4);
-  }
-  .rank-pannel {
-    margin:50px 0px 0px 10px;position: relative;float: left;
-  }
-
-  .text {
-    font-size: 14px;
-  }
-
-  .item {
-    margin-bottom: 18px;
-  }
-
   .clearfix:before,
   .clearfix:after {
     display: table;
     content: "";
   }
-
   .clearfix:after {
     clear: both
-  }
-
-  .box-card {
-    width: 768px;
-    margin-top: 10px;
   }
 </style>
 
@@ -128,7 +70,9 @@
     data() {
       return {
         state2: '',
-        activeIndex: '/home'
+        activeIndex: '/home',
+        rec_banners: [],
+        currentDate: new Date()
       }
     },
     components: {
@@ -138,31 +82,16 @@
       login() {
         this.$router.push({path: '/login'});
       },
-      querySearch(queryString, cb) {
-        var restaurants = this.restaurants;
-        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
-        // 调用 callback 返回建议列表的数据
-        cb(results);
-      },
-      createFilter(queryString) {
-        return (restaurant) => {
-          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-        };
-      }, loadAll() {
+      load_rec_banners() {
         return [
-          {"value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号"},
-          {"value": "红辣椒麻辣烫", "address": "上海市长宁区天山西路492号"},
-          {"value": "(小杨生煎)西郊百联餐厅", "address": "长宁区仙霞西路88号百联2楼"},
-          {"value": "阳阳麻辣烫", "address": "天山西路389号"},
-          {"value": "南拳妈妈龙虾盖浇饭", "address": "普陀区金沙江路1699号鑫乐惠美食广场A13"}
+          {"pic": "//puui.qpic.cn/media_img/lena/PICxpgz13_580_1680/0"},
+          {"pic": "//puui.qpic.cn/media_img/lena/PICd1loir_580_1680/0"},
+          {"pic": "//puui.qpic.cn/tv/0/48015925_1680580/0"},
         ];
-      },
-      handleSelect(item) {
-        console.log(item);
       }
     },
     mounted() {
-      this.restaurants = this.loadAll();
+      this.rec_banners = this.load_rec_banners();
     }
   }
 </script>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="navheader">
-    <el-container>
-      <el-header>
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          background-color="#545c64"
-          text-color="#fff"
-          v-bind:router = "true"
-          active-text-color="#ffd04b" >
-          <el-menu-item index="/home">LOGO</el-menu-item>
-          <!--<el-menu-item index="/helloWorld">helloWorld</el-menu-item>-->
-          <el-menu-item style="" index="/login">login</el-menu-item>
-        </el-menu>
-      </el-header>
-    </el-container>
-    <!--<div>-->
-      <!--<router-view/>-->
-    <!--</div>-->
+    <!--<el-container>-->
+      <!--<el-header>-->
+        <!--<el-menu-->
+          <!--:default-active="activeIndex"-->
+          <!--class="el-menu-demo"-->
+          <!--mode="horizontal"-->
+          <!--background-color="#545c64"-->
+          <!--text-color="#fff"-->
+          <!--v-bind:router = "true"-->
+          <!--active-text-color="#ffd04b" >-->
+          <!--<el-menu-item index="/home">LOGO</el-menu-item>-->
+          <!--&lt;!&ndash;<el-menu-item index="/helloWorld">helloWorld</el-menu-item>&ndash;&gt;-->
+          <!--<el-menu-item style="" index="/login">login</el-menu-item>-->
+        <!--</el-menu>-->
+      <!--</el-header>-->
+    <!--</el-container>-->
+    <div id="navs">
+      <div>
+        <img class="nav-logo" src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1224579036,3430335986&fm=58&s=37F76832DD66EE1149549AF40300D0B5&bpow=121&bpoh=75"/>
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
@@ -34,11 +37,17 @@
 </script>
 
 <style scoped>
+  #navs {
+    height: 50px;
+    background-color: rgba(128, 128, 128, 0.03);
+  }
+  .nav-logo {
+    height: 50px;
+  }
   #navheader {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: right;
     margin-top: 0px;
   }
 </style>
